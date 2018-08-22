e29fb39cbfde86265a596ab36ca97341c99af608
zydirtyfish <447207044@qq.com>
2018-08-22 10:28:38 +0800

    Create repository

---
+++ ColdSim.cpp
@@ -0,0 +1,7 @@
+#include "ColdSim.h"
+
+int main(int argc, char *argv[])
+{
+    cout << "1234" << endl;
+    return 0;
+}
---
+++ ColdSim.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <cstring>
+
+using namespace std;
