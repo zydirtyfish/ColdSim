e890a0fc1b9631485310e73ddbfde3d6d8344786
zydirtyfish <447207044@qq.com>
2018-08-30 12:36:25 +0800

    update run.h

--- run.h
+++ run.h
@@ -7,21 +7,50 @@ class RUN
 private:
     get_trace_tool *gtt;
     struct IoRecord *ior;
+    MemStruct *ms;
 
-public:
+  public:
     RUN()
     {
+        ms = new MemStruct();
+    }
 
+    ~RUN()
+    {
+        delete ms;
+        ms = NULL;
     }
     void exec()
     {
-        gtt = new get_trace_tool("filename");
+        /*gtt = new get_trace_tool("filename");
         ior = gtt->get_ti(true);
         while(ior != NULL)
         {
+            process(ior);
             ior = gtt->get_ti(true);
         }
-        delete gtt;
+        delete gtt;*/
+
+        ior = new IoRecord;
+        ior->alloc_time = 1514746000;
+        strcpy(ior->disksn, "12312aaaa");
+        ior->offset = 33435564;
+        ior->size = 16;
+        ior->io_type = 1;
+        process(ior);
     }
 
+    void process(struct IoRecord *ior)
+    {
+        /*处理流程示例*/
+        ms->isExpired(ior);
+        struct BlockStruct *bs = ms->ioToBlock(ior);
+        struct BlockStruct *bshead = bs;
+        while (bs != NULL)
+        {
+            ms->updateTOT(bs);
+            bs = bs->next;
+        }
+        ms->freeBlockList(bshead);
+    }
 };
